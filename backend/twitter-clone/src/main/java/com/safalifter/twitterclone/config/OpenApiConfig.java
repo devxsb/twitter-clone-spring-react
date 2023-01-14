acb1b62f6cb26cbb376b5d7fffea1fedc6a468cc
safalifter <70344201+safalifter@users.noreply.github.com>
2023-01-14 23:26:56 +0300

    open api added

---
+++ backend/twitter-clone/src/main/java/com/safalifter/twitterclone/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.safalifter.twitterclone.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version,
+                                 @Value("${application-license}") String license) {
+        return new OpenAPI()
+                .info(new Info().title("Twitter Clone API")
+                        .description(description)
+                        .license(new License().name(license))
+                        .version(version));
+    }
+}
