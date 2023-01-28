aaaf23958e5b8c1d18c4cd9ff3d62cb570f07f07
safalifter <70344201+safalifter@users.noreply.github.com>
2023-01-28 10:23:31 +0300

    exception handler improved

--- backend/twitter-clone/src/main/java/com/safalifter/twitterclone/exc/GeneralExceptionHandler.java
+++ backend/twitter-clone/src/main/java/com/safalifter/twitterclone/exc/GeneralExceptionHandler.java
@@ -1,32 +1,55 @@
 package com.safalifter.twitterclone.exc;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GeneralExceptionHandler {
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errorMap.put(error.getField(), error.getDefaultMessage())
-        );
-        return errorMap;
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(x -> errors.put(((FieldError) x).getField(), x.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public Map<String, String> handleNotFoundException(NotFoundException ex) {
-        Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("errorMessage", ex.getMessage());
-        return errorMap;
+    public ResponseEntity<?> notFoundException(NotFoundException exception) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", exception.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(WrongCredentialsException.class)
+    public ResponseEntity<?> usernameOrPasswordInvalidException(WrongCredentialsException exception) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", exception.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(GenericErrorResponse.class)
+    public ResponseEntity<?> genericError(GenericErrorResponse exception) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", exception.getMessage());
+        return new ResponseEntity<>(errors, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<?> handleAllException(Exception ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", ex.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ backend/twitter-clone/src/main/java/com/safalifter/twitterclone/exc/WrongCredentialsException.java
@@ -0,0 +1,12 @@
+package com.safalifter.twitterclone.exc;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class WrongCredentialsException extends RuntimeException {
+    public WrongCredentialsException(String message) {
+        super(message);
+    }
+}
