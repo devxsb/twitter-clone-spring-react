108d77b30c05c1a87be096cf163cc2b85f699885
safalifter <70344201+safalifter@users.noreply.github.com>
2023-01-30 12:37:40 +0300

    routes updated

--- frontend/twitter-clone/src/App.js
+++ frontend/twitter-clone/src/App.js
@@ -1,22 +1,35 @@
 import React from "react";
 import {useSelector} from "react-redux";
-import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
 import Login from "./layout/Login";
 import Signup from "./layout/Signup";
-import Home from "./pages/Home";
+import Tweet from "./pages/Tweet";
+import Container from "./layout/Container";
+import Sidebar from "./layout/Sidebar";
+import Content from "./layout/Content";
+import Widgets from "./layout/Widgets";
 
 const App = () => {
     const currentUser = useSelector(state => state.reduxSlice.currentUser)
     return (
-        <Routes>
-            <Route path='*' element={currentUser ? <Home/> : <Auth/>}/>
-            {!currentUser &&
-                <>
+        <>
+            {currentUser ?
+                <Container>
+                    <Sidebar/>
+                    <Routes>
+                        <Route path='*' element={<Content/>}/>
+                        <Route path="/tweets/:id" element={<Tweet/>}/>
+                    </Routes>
+                    <Widgets/>
+                </Container> :
+                <Routes>
+                    <Route path='*' element={<Home/>}/>
                     <Route path='/login' element={<Login/>}/>
                     <Route path='/signup' element={<Signup/>}/>
-                </>}
-        </Routes>
+                </Routes>
+            }
+        </>
     );
 };
 
