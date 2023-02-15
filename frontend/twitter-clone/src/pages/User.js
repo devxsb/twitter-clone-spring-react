bc77340dd9d685ccd9c55720258c0207f5a40262
safalifter <70344201+safalifter@users.noreply.github.com>
2023-02-15 17:24:58 +0300

    user tweets shown in user profile

--- frontend/twitter-clone/src/pages/User.js
+++ frontend/twitter-clone/src/pages/User.js
@@ -5,10 +5,11 @@ import bg from "../images/defaul-bg.png"
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Divider from "../components/Divider";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import FeedList from "../components/FeedList";
 
 const User = () => {
     const [user, setUser] = useState()
-
+    const [menu, setMenu] = useState(0) // 0 tweets 1 tweets & media 2 media 3 likes
     const {id} = useParams()
     const navigate = useNavigate()
 
@@ -46,7 +47,7 @@ const User = () => {
                             className="w-28 h-28 rounded-full mt-36 ml-3 absolute"
                         />
                     </div>
-                    <div className="h-52 ">
+                    <div className="mb-3">
                         <button
                             className="float-right h-10 bg-transparent py-2 px-4 rounded-2xl mt-1 mr-1 font-bold text-sm"
                             style={{color: "#1DA1F2", border: `1px solid #1DA1F2`}}>Edit profile
@@ -55,13 +56,40 @@ const User = () => {
                             <span className="font-bold block mb-0">{user.name}</span>
                             <span className="font-light block text-sm">@{user.username}</span>
                             <span className="block">{user.bio}</span>
-                            <span className="font-light block mt-2" style={{fontSize: "13px"}}>
+                            <span className="flex items-center font-light block mt-2" style={{fontSize: "13px"}}>
                                 <CalendarMonthIcon fontSize="small"
                                                    style={{color: "lightslategrey", marginRight: "3px"}}/>
                                 Joined {month[new Date(user.creationTimestamp).getUTCMonth()]} {new Date(user.creationTimestamp).getFullYear()}</span>
+                           <div className="flex items-center">
+                               0 <span className="mr-2 p-1 text-sm font-light">Following</span>
+                               0 <span className="text-sm p-1 font-light">Followers</span>
+                           </div>
                         </div>
                     </div>
+                    <div className="flex justify-around p-2">
+                        <button
+                            className="border-b placeholder-gray-dark bg-transparent focus:outline-none text-sm"
+                            onClick={() => setMenu(0)}>
+                            Tweets
+                        </button>
+                        <button
+                            className="border-b placeholder-gray-dark bg-transparent focus:outline-none text-sm"
+                            onClick={() => setMenu(1)}>
+                            Tweets & replies
+                        </button>
+                        <button
+                            className="border-b placeholder-gray-dark bg-transparent focus:outline-none text-sm"
+                            onClick={() => setMenu(2)}>
+                            Media
+                        </button>
+                        <button
+                            className="border-b placeholder-gray-dark bg-transparent focus:outline-none text-sm"
+                            onClick={() => setMenu(3)}>
+                            Likes
+                        </button>
+                    </div>
                     <Divider/>
+                    <FeedList tweets={user.tweets}/>
                 </>
             }
         </div>
